6d169e23954152eb587ec76fb11eb2a1dbb9a6aa
Suyash Jawale <suyashjawale245@gmail.com>
2025-11-13 03:16:32 -0800

    Added support for SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+@file:Suppress("DEPRECATION")
+
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -5,14 +8,16 @@ plugins {
 
 android {
     namespace = "com.tech.modelx"
-    compileSdk = 35
+    compileSdk {
+        version = release(36)
+    }
 
     defaultConfig {
         applicationId = "com.tech.modelx"
-        minSdk = 21
-        targetSdk = 35
-        versionCode = 5
-        versionName = "5.0"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 6
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,20 +41,22 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.exifinterface)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(libs.androidx.camera.core)
     implementation(libs.androidx.camera.camera2)
+    // If you want to additionally use the CameraX Lifecycle library
     implementation(libs.androidx.camera.lifecycle)
+    // If you want to additionally use the CameraX VideoCapture library
+    implementation(libs.androidx.camera.video)
+    // If you want to additionally use the CameraX View class
     implementation(libs.androidx.camera.view)
-
+    implementation("androidx.exifinterface:exifinterface:1.4.1")
 }
